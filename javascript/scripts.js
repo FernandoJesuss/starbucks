62dba352fc91aa413dfe1443ab755dfdfeaaa5ef
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-08-11 21:44:57 -0300

    Refactor:Code Improvements

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,10 +1,34 @@
-let img = document.querySelector(".starbucks")
-let circulo = document.querySelector(".circulo")
+
+        function toggleMenu() {
+            const hamburger = document.querySelector('.hamburger');
+            const navMenu = document.querySelector('.nav-menu');
+            
+            hamburger.classList.toggle('active');
+            navMenu.classList.toggle('active');
+        }
+        
+        document.querySelectorAll('.nav-menu a').forEach(link => {
+            link.addEventListener('click', () => {
+                const hamburger = document.querySelector('.hamburger');
+                const navMenu = document.querySelector('.nav-menu');
+                
+                hamburger.classList.remove('active');
+                navMenu.classList.remove('active');
+            });
+        });
+
+let img = document.querySelector(".starbucks");
+let circulo = document.querySelector(".circulo");
 
 function trocaImagem(endereco) {
-    img.src = endereco
+    img.src = endereco;
 }
 
 function trocaCor(cor) {
-    circulo.style.background = cor
+     const circulo = document.querySelector('.circulo');
+
+    circulo.style.background = cor;
 }
+
+window.trocaImagem = trocaImagem;
+window.trocaCor = trocaCor;
